f38d59067bc024724713b2beb9a2a1ea2dbcaa75
RxNunez <Nunez.ryan@gmail.com>
2017-10-13 16:06:30 -0700

    include user input

--- app/src/main/java/com/epicodus/feedme/FeedMeActivity.java
+++ app/src/main/java/com/epicodus/feedme/FeedMeActivity.java
@@ -1,13 +1,20 @@
 package com.epicodus.feedme;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class FeedMeActivity extends AppCompatActivity {
+    private TextView mInterestTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed_me);
+        mInterestTextView = (TextView) findViewById(R.id.interestTextView);
+        Intent intent = getIntent();
+        String interest = intent.getStringExtra("interest");
+        mInterestTextView.setText("Here is the information you requested: " + interest);
     }
 }
--- app/src/main/java/com/epicodus/feedme/MainActivity.java
+++ app/src/main/java/com/epicodus/feedme/MainActivity.java
@@ -5,21 +5,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private Button mFindInformationButton;
+    private EditText mInterestEditText
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        mInterestEditText = (EditText) findViewById(R.id.InterestEditText);
         mFindInformationButton = (Button) findViewById(R.id.findInformationButton);
         mFindInformationButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String interest = mInterestEditText.getText().toString();
                 Intent intent = new Intent(MainActivity.this, FeedMeActivity.class);
+                intent.putExtra("Interest", Interest);
                 startActivity(intent);
             }
         });
