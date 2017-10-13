952b6f05db5d8b1c144e9b6955d61a397a730955
RxNunez <Nunez.ryan@gmail.com>
2017-10-13 15:41:22 -0700

    include addition of FeedMe activity

---
+++ app/src/main/java/com/epicodus/feedme/FeedMeActivity.java
@@ -0,0 +1,13 @@
+package com.epicodus.feedme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FeedMeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feed_me);
+    }
+}
