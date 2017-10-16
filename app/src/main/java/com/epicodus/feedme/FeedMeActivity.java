950ff3b42771ff585c122eef5e0195b01c7a7f31
RxNunez <Nunez.ryan@gmail.com>
2017-10-15 22:55:41 -0700

    include butterknife and array adapter

--- app/src/main/java/com/epicodus/feedme/FeedMeActivity.java
+++ app/src/main/java/com/epicodus/feedme/FeedMeActivity.java
@@ -3,17 +3,45 @@ package com.epicodus.feedme;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+import static android.widget.AdapterView.*;
 
 public class FeedMeActivity extends AppCompatActivity {
-    public static final String TAG = FeedMeActivity.class.getSimpleName();
     private TextView mInterestTextView;
+    private ListView mListView;
+    private String[] information = new String[] {"Italy", "Mexico",
+            "Thailand", "Japan", "America", "Russia",
+            "Columbia", "Spain", "Australia", "Brazil",
+            "Korea", "Philipines", "Venezula",
+            "France", "Middle East"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed_me);
+        ButterKnife.bind(this);
+
+        mListView = (ListView) findViewById(R.id.listView);
         mInterestTextView = (TextView) findViewById(R.id.interestTextView);
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, information);
+        mListView.setAdapter(adapter);
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener()  {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String information = ((TextView)view).getText().toString();
+                Toast.makeText(FeedMeActivity.this, information, Toast.LENGTH_LONG).show();
+            }
+        });
         Intent intent = getIntent();
         String interest = intent.getStringExtra("interest");
         mInterestTextView.setText("Here is the information you requested: " + interest);
--- app/src/main/java/com/epicodus/feedme/MainActivity.java
+++ app/src/main/java/com/epicodus/feedme/MainActivity.java
@@ -7,30 +7,34 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
 import android.widget.TextView;
 import android.widget.Toast;
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 
 public class MainActivity extends AppCompatActivity {
-    private Button mFindInformationButton;
-    private EditText mInterestEditText;
-    private TextView mAppNameTextView;
+    @Bind(R.id.findInformationButton) Button mFindInformationButton;
+    @Bind(R.id.interestEditText) EditText mInterestEditText;
+    @Bind(R.id.appNameTextView) TextView mAppNameTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mAppNameTextView = (TextView) findViewById(R.id.appNameTextView);
+        ButterKnife.bind(this);
+
         Typeface PacificoFont = Typeface.createFromAsset(getAssets(), "fonts/Pacifico.ttf");
         mAppNameTextView.setTypeface(PacificoFont);
-        mInterestEditText = (EditText) findViewById(R.id.interestEditText);
-        mFindInformationButton = (Button) findViewById(R.id.findInformationButton);
+
         mFindInformationButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String interest = mInterestEditText.getText().toString();
-                Intent intent = new Intent(MainActivity.this, FeedMeActivity.class);
-                intent.putExtra("interest", interest);
-                startActivity(intent);
+            String interest = mInterestEditText.getText().toString();
+            Intent intent = new Intent(MainActivity.this, FeedMeActivity.class);
+            intent.putExtra("interest", interest);
+            startActivity(intent);
             }
         });
     }
