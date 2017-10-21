16540775e1c955ecf8e9bd622d1eb10b4d5151f4
RxNunez <Nunez.ryan@gmail.com>
2017-10-20 17:06:51 -0700

    include request and response

--- app/src/main/java/com/epicodus/feedme/YelpService.java
+++ app/src/main/java/com/epicodus/feedme/YelpService.java
@@ -1,10 +1,18 @@
 package com.epicodus.feedme;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.Response;
 
 public class YelpService {
 
@@ -24,6 +32,52 @@ public class YelpService {
 
         Call call = client.newCall(request);
         call.enqueue(callback);
+    }
+    public ArrayList<Foodtruck> processResults(Response response) {
+        ArrayList<Foodtruck> foodtrucks = new ArrayList<>();
+
+        try {
+            String jsonData = response.body().string();
+            JSONObject yelpJSON = new JSONObject(jsonData);
+            JSONArray businessesJSON = yelpJSON.getJSONArray("businesses");
+            for (int i = 0; i < businessesJSON.length(); i++) {
+                JSONObject foodJSON = businessesJSON.getJSONObject(i);
+                String name = foodJSON.getString("name");
+                String phone = foodJSON.optString("display_phone", "Phone not available");
+                String website = foodJSON.getString("url");
+                double rating = foodJSON.getDouble("rating");
+
+                String imageUrl = foodJSON.getString("image_url");
+
+                double latitude = (double) foodJSON.getJSONObject("coordinates").getDouble("latitude");
+
+                double longitude = (double) foodJSON.getJSONObject("coordinates").getDouble("longitude");
+
+                ArrayList<String> address = new ArrayList<>();
+                JSONArray addressJSON = foodJSON.getJSONObject("location")
+                        .getJSONArray("display_address");
+                for (int y = 0; y < addressJSON.length(); y++) {
+                    address.add(addressJSON.get(y).toString());
+                }
+
+                ArrayList<String> categories = new ArrayList<>();
+                JSONArray categoriesJSON = foodJSON.getJSONArray("categories");
+
+                for (int y = 0; y < categoriesJSON.length(); y++) {
+                    categories.add(categoriesJSON.getJSONObject(y).getString("title"));
+                }
+                Foodtruck foodtruck = new Foodtruck(name, phone, website, rating,
+                        imageUrl, address, latitude, longitude, categories);
+                foodtrucks.add(foodtruck);
+            }
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+        catch (JSONException e){
+            e.printStackTrace();
+        }
+        return foodtrucks;
     }
 
 }
